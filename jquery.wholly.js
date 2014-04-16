8482b03bd914e7982dcc776fd394f9e9aad5ea2c
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-04-16 17:01:10 +0000

    Fixed bug affecting tables using rowspan.

--- jquery.wholly.js
+++ jquery.wholly.js
@@ -3,11 +3,17 @@
  * https://github.com/gajus/wholly
  */
 (function ($) {
+    var debug = false;
+
     $.fn.wholly = function () {
         var calcRowWidth,
             calcTableWidth,
             generateTableIndexTemplate;
-            
+        
+        /**
+         * @param {Object} table jQuery selector referencing table.
+         * @returns {Number} Maximum number of columns across all the table rows.
+         */
         calcTableWidth = function (table) {
             var maxWidth = 0;
 
@@ -22,6 +28,10 @@
             return maxWidth;
         };
 
+        /**
+         * @param {Object} row jQuery selector referencing table row.
+         * @returns {Number} Number of cells in the row.
+         */
         calcRowWidth = function (row) {
             var width = 0;
 
@@ -34,6 +44,11 @@
             return width;
         };
 
+        /**
+         * @param {Number} width Number of cells.
+         * @param {Number} height Number of rows.
+         * @returns {Array} Table representation in a matrix ignoring rowspan and colspan.
+         */
         generateTableIndexTemplate = function (width, height) {
             var tableIndex = [],
                 rowWidth,
@@ -53,6 +68,87 @@
             return tableIndex;
         };
 
+        /**
+         * @param {Object} table jQuery selector referencing table.
+         * @param {Array} tableIndex Table matrix (index) produced using the generateTableIndexTemplate. This variable is changed by reference.
+         */
+        mapTableToIndex = function (table, tableIndex) {
+            var rows = table.find('tr');
+
+            // Iterate through each hypothetical table row.
+            $.each(tableIndex, function (y) {
+                var row = rows.eq(y),
+                    rowChildren = row.children(),
+                    cellIndex = 0;
+
+                if (debug) {
+                    console.groupCollapsed('Table row.', 'y:', y, 'rowChildren.length:', rowChildren.length);
+                }
+
+                // Iterate through each hypothetical table row column.
+                // $.each will make a copy of the array before iterating. Must use live array reference.
+                $.each(tableIndex[y], function (x) {
+                    var cell = tableIndex[y][x],
+                        colspan,
+                        rowspan,
+                        i,
+                        j,
+                        tempData;
+
+                    // Table matrix is iterated left to right, top to bottom. It might be that cell has
+                    // been assigned a value already because previous row-cell had "rowspan" property,
+                    // possibly together with "colspan".
+                    if (cell) {
+                        if (debug) {
+                            console.log('x:', cellIndex, 'cell:', cell[0]);
+                        }
+                    } else {
+                        cell = rowChildren.eq(cellIndex++);
+
+                        colspan = parseInt(cell.attr('colspan'), 10) || 1;
+                        rowspan = parseInt(cell.attr('rowspan'), 10) || 1;
+
+                        if (debug) {
+                            if (colspan > 1 || rowspan > 1) {
+                                console.group('x:', x, 'colspan:', colspan, 'rowspan:', rowspan, 'cell:', cell[0]);
+                            } else {
+                                console.log('x:', x, 'colspan:', colspan, 'rowspan:', rowspan, 'cell:', cell[0]);
+                            }                            
+                        }
+
+                        for (i = 0; i < rowspan; i++) {
+                            for (j = 0; j < colspan; j++) {
+                                if (debug) {
+                                    console.log('relative row:', i, 'relative cell:', j, 'absolute row:', y + i, 'absolute cell:', x + j);
+                                }
+                                
+                                tableIndex[y + i][x + j] = cell;
+                            }
+                        }
+
+                        if (colspan > 1 || rowspan > 1) {
+                            console.groupEnd();
+                        }
+                    }
+
+                    tempData = cell.data('wholly.index');
+
+                    if (typeof tempData === 'undefined') {
+                        cell.data('wholly.index', x);
+                    }
+
+                    /*tempData = tempData || {};
+                    tempData[y] = typeof tempData[y] === 'undefined' ? x : tempData[y];
+
+                    cell.data('wholly.index', tempData);*/
+                });
+
+                if (debug) {
+                    console.groupEnd();
+                }
+            });
+        };
+
         return this.each(function () {
             var table = $(this),
                 tableWidth = calcTableWidth(table),
@@ -60,58 +156,19 @@
                 tableIndex = generateTableIndexTemplate(tableWidth, tableHeight),
                 column;
 
-            //console.log( tableIndex, tableIndex.length );
-            //console.log('tableWidth:', tableWidth, 'tableHeight:', tableHeight);
+            if (debug) {
+                console.log('tableWidth:', tableWidth, 'tableHeight:', tableHeight);
+            }
 
-            $.each(tableIndex, function (y) {
-                var row = table.find('tr').eq(y),
-                    rowChildren = row.children(),
-                    cellIndex = 0;
-
-                //console.log('row.length:', row.length, 'y:', y);
-
-                // Becareful. $.each will make a copy of the array before iterating. Must use live array reference.
-                $.each(tableIndex[y], function (x) {
-                    var cell = tableIndex[y][x],
-                        colspan,
-                        rowspan,
-                        i,
-                        j;
-
-                    if (!cell) {
-                        cell = rowChildren.eq(cellIndex++);
-
-                        //console.log('x:', cellIndex, 'cell:', cell.length);
-
-                        colspan = parseInt(cell.attr('colspan'), 10) || 1;
-                        rowspan = parseInt(cell.attr('rowspan'), 10) || 1;
-
-                        //console.log('rowChildren:', rowChildren.length, 'x:', x, 'colspan:', colspan, 'rowspan:', rowspan);
-
-                        for (i = 0; i < rowspan; i++) {
-                            for (j = 0; j < colspan; j++) {
-                                //console.log('i:', i, 'j:', j, 'yi:', y + i, 'xj:', x + j);
-                                
-                                tableIndex[y + i][x + j] = cell;
-                            }
-                        }
-                    }
-                });
-            });
+            mapTableToIndex(table, tableIndex);
             
             table.on('mouseenter', 'td, th', function () {
-                var cellRealIndex = 0,
-                    colspan = parseInt($(this).attr('colspan'), 10) || 1,
+                var colspan = parseInt($(this).attr('colspan'), 10) || 1,
+                    cellRealIndex = $(this).data('wholly.index'),
                     highlightCellFrom,
                     highlightCellTo;
 
                 column = $(this);
-
-                $(this).prevAll().each(function () {
-                    var colspan = parseInt($(this).attr('colspan'), 10) || 1;
-
-                    cellRealIndex += colspan;
-                });
 
                 highlightCellFrom = cellRealIndex;
                 highlightCellTo = cellRealIndex + colspan;
